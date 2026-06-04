bf860658b683ad533ccafdb929c57a3d1b8027e0
Sascha Lucius <sascha.lucius@staffbase.com>
2026-06-04 17:41:59 +0200

    warnings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -50,10 +49,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     buildFeatures {
